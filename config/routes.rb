ba5193c07fdbf9c9dbdb45d9664d7479e95440cb
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-11-01 15:42:25 +0000

    routes changed to use /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :bookings
+  # APi routes should be in /api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :bookings
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
