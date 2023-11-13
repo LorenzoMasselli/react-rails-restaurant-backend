099c8772f295127adbaefafe1f83a40f96f4f1a2
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-11-13 12:38:30 +0000

    changed origins in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Where the request comes from
-    origins "http://127.0.0.1:5173"
+    # origins "http://127.0.0.1:5173"
+    origins "http://127.0.0.1:5174"
 
     resource "*",
     headers: ["Authorization"],
