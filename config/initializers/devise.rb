d46d063640f697da9584d7ec5681cddad90c71c8
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2023-12-04 15:29:15 +0000

    changed CORS origins for local

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -322,6 +322,6 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 5.minutes.to_i
+    jwt.expiration_time = 30.minutes.to_i
   end
 end
