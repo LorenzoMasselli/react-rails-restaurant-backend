5f578531e1329ac1bb0eba5c2dc4c91baf78742d
Lorenzo Masselli <lorenzomasselli1@gmail.com>
2024-01-05 09:39:47 +0400

    changed seeds and origins in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins "https://lorenzomasselli.github.io"
-    origins "http://127.0.0.1:5173"
+    origins "https://lorenzomasselli.github.io"
+    # origins "http://127.0.0.1:5173"
 
     resource "*",
     headers: ["Authorization"],
